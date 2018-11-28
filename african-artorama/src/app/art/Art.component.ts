1355debcb45cebb1791b3832f9ebaeb49b5942d2
Mike Detweiler <mkdetweiler@gmail.com>
2018-11-28 12:36:35 -0700

    Adjusted look and feel

--- african-artorama/src/app/art/Art.component.ts
+++ african-artorama/src/app/art/Art.component.ts
@@ -2,14 +2,10 @@ import { Component, Input } from '@angular/core';
 import { ArtPiece } from './ArtPiece';
 
 @Component({
-  selector: 'art',
+  selector: 'app-art',
   templateUrl: './Art.component.html',
-  styles: [`
-    p{
-      text-align: right;
-    }
-  `]
+  styles: ['p{ text-align:right; }', '.pic{ height:200px }']
 })
 export class ArtComponent {
-  @Input() art: ArtPiece
+  @Input() art: ArtPiece;
 }
